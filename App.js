2bd81d85cb364dcdd6e15a3abacf12f7dca1469f
Talhaarain666 <83097144+Talhaarain666@users.noreply.github.com>
2022-05-30 08:46:28 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import AppRouting from './config/routing';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <AppRouting />
+      </header>
+    </div>
+  );
+}
+
+export default App;
